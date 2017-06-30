ee8749a86a2b493c4814b5ec76c26f25460fa253
Matias Turunen <matias.turunen@gmail.com>
2017-06-30 22:12:07 +0300

    Updates to everything

---
+++ C++/Chapter_2/Calc.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+main()
+{
+    int number_1;
+    int number_2;
+    int sum;
+
+    cout << "Type the first number:";
+    cin >> number_1;
+    cout << "Type the second number:";
+    cin >> number_2;
+    sum = number_1 + number_2;
+    cout << "Sum of the numbers is " << sum << endl;
+}
